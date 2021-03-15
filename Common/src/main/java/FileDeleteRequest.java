e8f14b288b0b3675b2ceb44be233d75e7b21ad08
Strelok90 <new_fantom5@mail.ru>
2021-03-15 18:17:00 +0300

    Сделано соединение с сервером

---
+++ Common/src/main/java/FileDeleteRequest.java
@@ -0,0 +1,14 @@
+
+
+
+public class FileDeleteRequest extends AbstractMessage {
+    private String fileName;
+
+    public FileDeleteRequest(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+}
