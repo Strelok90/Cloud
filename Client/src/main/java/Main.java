c3975779824b8124258a1a6b6b596d37a533425c
Strelok90 <new_fantom5@mail.ru>
2021-03-15 19:01:15 +0300

    Сделано соединение с сервером

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -10,11 +10,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("File Manager");
+        primaryStage.setTitle("BlackBox");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
         primaryStage.setOnCloseRequest(event -> {
-            // Controller.stop();
             Platform.exit();
         });
     }
